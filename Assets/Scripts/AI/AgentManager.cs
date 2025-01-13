597306b8eec8be7e36a3d57167d332f7bafd8f1e
Phoenix <Cedarbb@gmail.com>
2025-01-13 09:59:55 -0800

    Changes for presintation

--- Assets/Scripts/AI/AgentManager.cs
+++ Assets/Scripts/AI/AgentManager.cs
@@ -53,7 +53,7 @@ public class AgentManager : MonoBehaviour
 
         for(int i = 0; i < childrenPerGeneration; i++)
         {
-            Instantiate(agentPrefab, parentPos, Quaternion.identity);
+            Instantiate(agentPrefab, new Vector3(parentPos.x, 0, 0), Quaternion.identity);
         }
     }
 
